df041bfc4b1b4cde8afc63e89731629af3fc8cb7
chaodongwen <944359632@qq.com>
2023-06-05 19:42:06 +0800

    init

---
+++ pytorch_code/SRMAFKG.py
@@ -0,0 +1,387 @@
+import datetime
+import math
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import Module, Parameter
+import torch.nn.functional as F
+import pandas as pd
+import dgl
+from dgl.nn.pytorch.softmax import edge_softmax
+
+def _L2_loss_mean(x):
+    return torch.mean(torch.sum(torch.pow(x, 2), dim=1, keepdim=False) / 2.)
+
+#聚合器
+class Aggregator(nn.Module):
+    def __init__(self, in_dim, out_dim, dropout, aggregator_type):
+        super(Aggregator, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.dropout = dropout
+        self.aggregator_type = aggregator_type
+        self.message_dropout = nn.Dropout(dropout)
+        if aggregator_type == 'gcn':
+            self.W = nn.Linear(self.in_dim, self.out_dim)       # W in Equation (6)
+        elif aggregator_type == 'graphsage':
+            self.W = nn.Linear(self.in_dim * 2, self.out_dim)   # W in Equation (7)
+        elif aggregator_type == 'bi-interaction':
+            self.W1 = nn.Linear(self.in_dim, self.out_dim)      # W1 in Equation (8)
+            self.W2 = nn.Linear(self.in_dim, self.out_dim)      # W2 in Equation (8)
+        else:
+            raise NotImplementedError  # 抛出异常
+        self.activation = nn.LeakyReLU()
+
+    def forward(self, mode, g, entity_embed):
+        g = g.local_var()
+        g.ndata['node'] = entity_embed
+
+        # Equation (3) & (10)
+        # DGL: dgl-cu10.1(0.5.3)
+        # Get different results when using `dgl.function.sum`, and the randomness is due to `atomicAdd`
+        # Use `dgl.function.sum` when training model to speed up
+        # Use custom function to ensure deterministic behavior when predicting
+        if mode == 'predict':
+            g.update_all(dgl.function.u_mul_e('node', 'att', 'side'), lambda nodes: {'N_h': torch.sum(nodes.mailbox['side'], 1)})
+        else:
+            # side = 节点编码向量 @ 知识意识注意， N_h = sum(side)
+            g.update_all(dgl.function.u_mul_e('node', 'att', 'side'), dgl.function.sum('side', 'N_h'))
+
+        if self.aggregator_type == 'gcn':
+            # Equation (6) & (9)
+            out = self.activation(self.W(g.ndata['node'] + g.ndata['N_h']))                         # (n_users + n_entities, out_dim)
+
+        elif self.aggregator_type == 'graphsage':
+            # Equation (7) & (9)
+            out = self.activation(self.W(torch.cat([g.ndata['node'], g.ndata['N_h']], dim=1)))      # (n_users + n_entities, out_dim)
+
+        elif self.aggregator_type == 'bi-interaction':
+            # Equation (8) & (9)
+            out1 = self.activation(self.W1(g.ndata['node'] + g.ndata['N_h']))                       # (n_users + n_entities, out_dim)
+            out2 = self.activation(self.W2(g.ndata['node'] * g.ndata['N_h']))                       # (n_users + n_entities, out_dim)
+            out = out1 + out2
+        else:
+            raise NotImplementedError
+
+        out = self.message_dropout(out)
+        return out
+
+
+class KGAT(nn.Module):                   #KGAT层
+    def __init__(self, args,
+                 n_users, n_entities, n_relations,
+                 user_pre_embed=None, item_pre_embed=None):
+
+        super(KGAT, self).__init__()
+        self.use_pretrain = args.use_pretrain
+
+        self.n_users = n_users
+        self.n_entities = n_entities
+        self.n_relations = n_relations
+
+        self.entity_dim = args.entity_dim
+        self.relation_dim = args.relation_dim
+
+        self.aggregation_type = args.aggregation_type
+        self.conv_dim_list = [args.entity_dim] + eval(args.conv_dim_list)  # 层的维度
+        self.mess_dropout = eval(args.mess_dropout)  # layers dropout
+        self.n_layers = len(eval(args.conv_dim_list))  # 层数
+
+        self.kg_l2loss_lambda = args.kg_l2loss_lambda
+        self.cf_l2loss_lambda = args.cf_l2loss_lambda
+
+        # Embedding
+        self.relation_embed = nn.Embedding(self.n_relations, self.relation_dim)
+        self.entity_user_embed = nn.Embedding(self.n_entities + self.n_users, self.entity_dim)
+        if (self.use_pretrain == 1) and (user_pre_embed is not None) and (item_pre_embed is not None):
+            other_entity_embed = nn.Parameter(torch.Tensor(self.n_entities - item_pre_embed.shape[0], self.entity_dim))
+            nn.init.xavier_uniform_(other_entity_embed, gain=nn.init.calculate_gain('relu'))  # 均匀分布
+            entity_user_embed = torch.cat([item_pre_embed, other_entity_embed, user_pre_embed], dim=0)
+            self.entity_user_embed.weight = nn.Parameter(entity_user_embed)
+
+        # 注意嵌入传播层的W [n_relations, entity_dim, relation_dim]
+        self.W_R = nn.Parameter(torch.Tensor(self.n_relations, self.entity_dim, self.relation_dim))
+        nn.init.xavier_uniform_(self.W_R, gain=nn.init.calculate_gain('relu'))
+
+        # 聚合层
+        self.aggregator_layers = nn.ModuleList()
+        for k in range(self.n_layers):
+            self.aggregator_layers.append(Aggregator(self.conv_dim_list[k], self.conv_dim_list[k + 1], self.mess_dropout[k], self.aggregation_type))
+
+    def att_score(self, edges):
+        # Equation (4)
+        r_mul_t = torch.matmul(self.entity_user_embed(edges.src['id']), self.W_r)                       # (n_edge, relation_dim)
+        r_mul_h = torch.matmul(self.entity_user_embed(edges.dst['id']), self.W_r)                       # (n_edge, relation_dim)
+        r_embed = self.relation_embed(edges.data['type'])                                               # (1, relation_dim)
+        att = torch.bmm(r_mul_t.unsqueeze(1), torch.tanh(r_mul_h + r_embed).unsqueeze(2)).squeeze(-1)   # (n_edge, 1)
+        return {'att': att}
+
+    def compute_attention(self, g):
+        g = g.local_var()  # 返回局部变量的图形对象，避免在退出函数时更改图形特征
+        for i in range(self.n_relations):
+            edge_idxs = g.filter_edges(lambda edge: edge.data['type'] == i)  # 返回满足给定边类型的边ID
+            self.W_r = self.W_R[i]  # [entity_dim, relation_dim]
+            g.apply_edges(self.att_score, edge_idxs)  # 添加新的边缘特征，也就是文章中的pi值，Knowledge-aware Attention
+
+        # Equation (5)
+        g.edata['att'] = edge_softmax(g, g.edata.pop('att'))
+        return g.edata.pop('att')
+
+    def calc_kg_loss(self, h, r, pos_t, neg_t):
+        """
+        h:      (kg_batch_size)
+        r:      (kg_batch_size)
+        pos_t:  (kg_batch_size)
+        neg_t:  (kg_batch_size)
+        """
+        r_embed = self.relation_embed(r)                 # (kg_batch_size, relation_dim)
+        W_r = self.W_R[r]                                # (kg_batch_size, entity_dim, relation_dim)
+
+        h_embed = self.entity_user_embed(h)              # (kg_batch_size, entity_dim)
+        pos_t_embed = self.entity_user_embed(pos_t)      # (kg_batch_size, entity_dim)
+        neg_t_embed = self.entity_user_embed(neg_t)      # (kg_batch_size, entity_dim)
+
+        r_mul_h = torch.bmm(h_embed.unsqueeze(1), W_r).squeeze(1)             # (kg_batch_size, relation_dim)
+        r_mul_pos_t = torch.bmm(pos_t_embed.unsqueeze(1), W_r).squeeze(1)     # (kg_batch_size, relation_dim)
+        r_mul_neg_t = torch.bmm(neg_t_embed.unsqueeze(1), W_r).squeeze(1)     # (kg_batch_size, relation_dim)
+
+        # Equation (1)
+        pos_score = torch.sum(torch.pow(r_mul_h + r_embed - r_mul_pos_t, 2), dim=1)     # (kg_batch_size)
+        neg_score = torch.sum(torch.pow(r_mul_h + r_embed - r_mul_neg_t, 2), dim=1)     # (kg_batch_size)
+
+        # Equation (2)
+        kg_loss = (-1.0) * F.logsigmoid(neg_score - pos_score)
+        kg_loss = torch.mean(kg_loss)
+
+        l2_loss = _L2_loss_mean(r_mul_h) + _L2_loss_mean(r_embed) + _L2_loss_mean(r_mul_pos_t) + _L2_loss_mean(r_mul_neg_t)
+        loss = kg_loss + self.kg_l2loss_lambda * l2_loss
+        return loss
+
+    def cf_embedding(self, mode, g):
+        g = g.local_var()
+        ego_embed = self.entity_user_embed(g.ndata['id'])
+        all_embed = [ego_embed]
+
+        # 高阶传播
+        for i, layer in enumerate(self.aggregator_layers):
+            ego_embed = layer(mode, g, ego_embed)
+            norm_embed = F.normalize(ego_embed, p=2, dim=1)
+            all_embed.append(norm_embed)
+
+        # Equation (11)  串联运算
+        all_embed = torch.cat(all_embed, dim=1)         # (n_users + n_entities, cf_concat_dim)
+        return all_embed
+
+    def cf_score(self, mode, g, user_ids, item_ids):
+        """
+        user_ids:   number of users to evaluate   (n_eval_users)
+        item_ids:   number of items to evaluate   (n_eval_items)
+        """
+        all_embed = self.cf_embedding(mode, g)          # (n_users + n_entities, cf_concat_dim)
+        user_embed = all_embed[user_ids]                # (n_eval_users, cf_concat_dim)
+        item_embed = all_embed[item_ids]                # (n_eval_items, cf_concat_dim)
+
+        # Equation (12)
+        cf_score = torch.matmul(user_embed, item_embed.transpose(0, 1))    # (n_eval_users, n_eval_items)
+        return cf_score
+
+    def calc_cf_loss(self, mode, g, user_ids, item_pos_ids, item_neg_ids):
+        """
+        user_ids:       (cf_batch_size)
+        item_pos_ids:   (cf_batch_size)
+        item_neg_ids:   (cf_batch_size)
+        """
+        all_embed = self.cf_embedding(mode, g)                      # (n_users + n_entities, cf_concat_dim)
+        user_embed = all_embed[user_ids]                            # (cf_batch_size, cf_concat_dim)
+        item_pos_embed = all_embed[item_pos_ids]                    # (cf_batch_size, cf_concat_dim)
+        item_neg_embed = all_embed[item_neg_ids]                    # (cf_batch_size, cf_concat_dim)
+
+        # Equation (12)
+        pos_score = torch.sum(user_embed * item_pos_embed, dim=1)   # (cf_batch_size)
+        neg_score = torch.sum(user_embed * item_neg_embed, dim=1)   # (cf_batch_size)
+
+        # Equation (13)
+        cf_loss = (-1.0) * F.logsigmoid(pos_score - neg_score)
+        cf_loss = torch.mean(cf_loss)
+
+        # 正则化
+        l2_loss = _L2_loss_mean(user_embed) + _L2_loss_mean(item_pos_embed) + _L2_loss_mean(item_neg_embed)
+        loss = cf_loss + self.cf_l2loss_lambda * l2_loss
+        return loss
+
+    def forward(self, mode, *input):
+        if mode == 'calc_att':
+            return self.compute_attention(*input)
+        if mode == 'calc_cf_loss':
+            return self.calc_cf_loss(mode, *input)
+        if mode == 'calc_kg_loss':
+            return self.calc_kg_loss(*input)
+        if mode == 'predict':
+            return self.cf_score(mode, *input)
+
+#图神经网络
+class GNN(Module):
+    def __init__(self, hidden_size, step=1):
+        super(GNN, self).__init__()
+        self.step = step
+        self.hidden_size = hidden_size
+        self.input_size = hidden_size * 2
+        self.gate_size = 3 * hidden_size
+        self.w_ih = Parameter(torch.Tensor(self.gate_size, self.input_size))
+        self.w_hh = Parameter(torch.Tensor(self.gate_size, self.hidden_size))
+        self.b_ih = Parameter(torch.Tensor(self.gate_size))
+        self.b_hh = Parameter(torch.Tensor(self.gate_size))
+        self.b_iah = Parameter(torch.Tensor(self.hidden_size))
+        self.b_oah = Parameter(torch.Tensor(self.hidden_size))
+        self.linear_edge_in = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_edge_out = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_edge_f = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+
+    def GNNCell(self, A, hidden):
+        input_in = torch.matmul(A[:, :, :A.shape[1]], self.linear_edge_in(hidden)) + self.b_iah
+        input_out = torch.matmul(A[:, :, A.shape[1]: 2 * A.shape[1]], self.linear_edge_out(hidden)) + self.b_oah
+        inputs = torch.cat([input_in, input_out], 2)
+
+        gi = F.linear(inputs, self.w_ih, self.b_ih)
+        gh = F.linear(hidden, self.w_hh, self.b_hh)
+
+        i_r, i_i, i_n = gi.chunk(3, 2)
+        h_r, h_i, h_n = gh.chunk(3, 2)
+
+        resetgate = torch.sigmoid(i_r + h_r)
+        inputgate = torch.sigmoid(i_i + h_i)
+
+        newgate = torch.tanh(i_n + resetgate * h_n)
+
+        hy = newgate + inputgate * (hidden - newgate)
+        return hy
+
+
+    def forward(self, A, hidden):
+        for i in range(self.step):
+            hidden = self.GNNCell(A, hidden)
+        return hidden
+
+
+class SRMAFKG(Module):
+    def __init__(self, opt, n_node):                #opt是lr batchsize等参数   n_node是item_dict长度
+        super(SRMAFKG, self).__init__()
+        self.hidden_size = opt.hiddenSize
+        self.n_node = n_node
+        self.batch_size = opt.batchSize
+        self.nonhybrid = opt.nonhybrid
+        self.embedding = nn.Embedding(self.n_node, self.hidden_size)
+        self.gnn = GNN(self.hidden_size, step=opt.step)
+        # Item representation & Position representation
+        self.embedding = nn.Embedding(self.n_node, self.hidden_size)
+        self.pos_embedding = nn.Embedding(100, self.hidden_size)
+        self.kgat = KGAT(self.hidden_size, step=opt.step)
+        self.linear_one = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_two = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_three = nn.Linear(self.hidden_size, 1, bias=False)
+        # linear_transform
+        self.linear_transform = nn.Linear(self.hidden_size * 2, self.hidden_size, bias=True)
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr, weight_decay=opt.l2)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=opt.lr_dc_step, gamma=opt.lr_dc)
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def compute_scores(self, hidden, mask):
+
+        ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
+        #KGAT的embding
+        kgat_embding = hidden[1]
+        #聚合
+        ht =  torch.cat(ht,kgat_embding)
+
+        q1 = self.linear_one(ht).view(ht.shape[0], 1, ht.shape[1])  # batch_size x 1 x latent_size
+        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
+        alpha = self.linear_three(torch.sigmoid(q1 + q2))
+        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)
+        if not self.nonhybrid:
+            a = self.linear_transform(torch.cat([a, ht], 1))
+        b = self.embedding.weight[1:]  # n_nodes x latent_size
+        scores = torch.matmul(a, b.transpose(1, 0))
+        return scores
+
+    def forward(self, inputs, A):
+        hidden = self.embedding(inputs)
+        hidden1 = self.kgat(A, hidden)
+        hidden = self.gnn(A, hidden)
+        return [hidden,hidden1]
+
+
+def trans_to_cuda(variable):
+    if torch.cuda.is_available():
+        return variable.cuda()
+    else:
+        return variable
+
+
+def trans_to_cpu(variable):
+    if torch.cuda.is_available():
+        return variable.cpu()
+    else:
+        return variable
+
+
+def forward(model, i, data):
+    alias_inputs, A, items, mask, targets = data.get_slice(i)
+    alias_inputs = trans_to_cuda(torch.Tensor(alias_inputs).long())
+    items = trans_to_cuda(torch.Tensor(items).long())
+    A = trans_to_cuda(torch.Tensor(A).float())
+    mask = trans_to_cuda(torch.Tensor(mask).long())
+    hidden = model(items, A)
+    get = lambda i: hidden[i][alias_inputs[i]]
+    seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
+
+    return targets, model.compute_scores(seq_hidden, mask)
+
+
+def train_test(model, train_data, test_data):
+    # flag = False
+    print(model)
+    model.scheduler.step()
+    print('start training: ', datetime.datetime.now())
+    model.train()
+    # best_loss = float('inf')
+    # total_batch = 0
+    total_loss = 0.0
+    slices = train_data.generate_batch(model.batch_size)
+    for i, j in zip(slices, np.arange(len(slices))):
+        model.optimizer.zero_grad()
+        targets, scores = forward(model, i, train_data)                             #对训练集的数据进行前向计算
+        targets = trans_to_cuda(torch.Tensor(targets).long())
+        loss = model.loss_function(scores, targets - 1)
+        loss.backward()
+        model.optimizer.step()
+        total_loss += loss
+        if j % int(len(slices) / 5 + 1) == 0:
+            # if total_loss < best_loss:
+            #     best_loss = total_loss
+            print('[%d/%d] Loss: %.4f' % (j, len(slices), loss.item()))
+
+    print('\tLoss:\t%.3f' % total_loss)
+    print('start predicting: ', datetime.datetime.now())
+    model.eval()
+    hit, mrr = [], []
+    slices = test_data.generate_batch(model.batch_size)
+    for i in slices:
+        targets, scores = forward(model, i, test_data)
+        sub_scores = scores.topk(20)[1]
+        sub_scores = trans_to_cpu(sub_scores).detach().numpy()
+        for score, target, mask in zip(sub_scores, targets, test_data.mask):
+            hit.append(np.isin(target - 1, score))
+            if len(np.where(score == target - 1)[0]) == 0:
+                mrr.append(0)
+            else:
+                mrr.append(1 / (np.where(score == target - 1)[0][0] + 1))
+    hit = np.mean(hit) * 100
+    mrr = np.mean(mrr) * 100
+    return hit, mrr
